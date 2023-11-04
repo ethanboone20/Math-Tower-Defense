8d44476bfb1ce4e028e183f8b284d22edab1b104
CaptainCaptain05 <aobrienapex@gmail.com>
2023-11-04 14:54:19 -0400

    Main Menu scene added
    Added main menu scene, some images for that as well as game scene ("Grassy Plains") UI elements. Also added some mp3 files for music, made some changes to main menu for it to work properly. Added play game script, made changes to the script.

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Grassy Plains");
+
+    }
+
+    
+}
+
